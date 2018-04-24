5059f32041cc975d49085670b8d9c0bed74dae5c
dmitry.shatalov <shatal1@yandex.ru>
2018-04-24 17:31:16 +0700

    added redirect to login page in AuthGuard

--- Photoalbum/src/app/shared-module/services/auth.guard.service.ts
+++ Photoalbum/src/app/shared-module/services/auth.guard.service.ts
@@ -1,11 +1,16 @@
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService){}
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-        return this.authService.isLoggedIn();
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(["/login"]);
+      return false;
     }
+  }
 }
