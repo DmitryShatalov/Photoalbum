af49ca9d0d9f289c719e95d46346bfcfc59d8ef1
dmitry.shatalov <shatal1@yandex.ru>
2018-05-15 17:16:39 +0700

    small code changes

--- Photoalbum/src/app/comments/comments-wrapper/comments-wrapper.component.ts
+++ Photoalbum/src/app/comments/comments-wrapper/comments-wrapper.component.ts
@@ -17,7 +17,7 @@ export class CommentsWrapperComponent implements OnInit {
 
   addComment(comment){
     this.commentsService.addComment(comment).subscribe(res => {
-      this.commentsService.getCommentsByPhotoId(this.post.photoId).subscribe(res => console.log(res))
+     // this.commentsService.getCommentsByPhotoId(this.post.photoId).subscribe(res => console.log(res))
     })
   }
 }
